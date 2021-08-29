4e8936b484e9f450d923440b45570e9b35ac8efa
Adam M. Wilson <adamw@buffalo.edu>
2021-08-29 18:38:11 -0400

    update tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,12 @@
 
 
 library(testthat)
-url="https://raw.githubusercontent.com/AdamWilsonLabEDU/GEO511_CS01/main/tests/testthat/tests.R"
+
+# Link to raw version of tests
+# This is done so the student repositories always pull the most recent version of the test
+# Otherwise they would have to pull the updated version every time I make a change.
+url="http://raw.githubusercontent.com/AdamWilsonLabEDU/GEO511_CS01/main/tests/testthat/tests.R"
+
+# now run the updated version directly from the file
 source(url)
+#test_file(path = "tests/tests2.R")
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -4,12 +4,22 @@
 #}
 # library(testthat)
 # 
-# url="https://raw.githubusercontent.com/AdamWilsonLabEDU/GEO511_CS01/main/tests/testthat/tests.R"
-# 
-# download.file(url = paste0(repo,"/main/tests/testthat/tests.R"),
-#               destfile="tests/testthat.R")
+#url="https://raw.githubusercontent.com/AdamWilsonLabEDU/GEO511_CS01/main/tests/testthat/tests.R"
+ 
+#download.file(url = paste0(repo,"/main/tests/testthat/tests.R"),
+#               destfile="tests/tests2.R")
 # 
 # setwd("..")
 # 
 # url="https://raw.githubusercontent.com/AdamWilsonLabEDU/GEO511_CS01/main/tests/testthat/tests.R"
 # source(url)
+
+#library(testthat)
+
+# Link to raw version of tests
+# This is done so the student repositories always pull the most recent version of the test
+# Otherwise they would have to pull the updated version every time I make a change.
+#url="http://raw.githubusercontent.com/AdamWilsonLabEDU/GEO511_CS01/main/tests/testthat/tests.R"
+
+# now run the updated version directly from the file
+#source(url)

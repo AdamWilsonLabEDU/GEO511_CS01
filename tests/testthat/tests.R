f533e3d06d2185be662de7b60db59ecb0f691550
Adam M. Wilson <adamw@buffalo.edu>
2021-08-29 18:36:29 -0400

    remove test_dir

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,6 +1,6 @@
 
 # run all tests in the testhat folder
-test_dir("tests/testthat/",stop_on_failure = FALSE,reporter = "progress")
+#test_dir("tests/testthat/",stop_on_failure = FALSE,reporter = "progress")
 
 
 # Case study script

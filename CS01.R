a85f9ddfd14e7efe5c7b76dc4c5a2364f2620376
adammwilson <900623+adammwilson@users.noreply.github.com>
2025-06-23 11:41:28 -0400

    udpate tests

--- CS01.R
+++ CS01.R
@@ -4,6 +4,9 @@ petal_length_mean=mean(iris$Petal.Length)
 #print(petal_length_mean)
 
 
-if(F) 
-  testthat::test_file("tests/testthat.R")
-  testthat::test_dir("tests")
+if(F) {
+#test_results =  testthat::test_file("tests/testthat.R", reporter = "summary")
+test_results =  testthat::test_dir("tests",reporter = "summary")
+test_results
+write_csv(as_tibble(test_results), "tests/test_results.csv")
+} 
--- tests/testthat.R
+++ tests/testthat.R
@@ -9,11 +9,17 @@ test_that(paste0("Your script file, (",file, ") exists"),{
 })
 
 context("File sources without error")
+
+test_that(paste0("Your script file, (",file, ") sources without errors"),{
   expect_no_error(source(file))
+  })
 
 context("Results")
-test_that("petal_length_mean",{
+
+test_that("1 petal_length_mean exists",{
   expect_true(exists("petal_length_mean"))
-  expect_equal(petal_length_mean, 3.758)
 })
 
+test_that("1 Mean is calculated correctly",{
+  expect_equal(petal_length_mean, 3.758)
+})
